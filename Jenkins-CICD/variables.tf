a9ce42f5d56a0402a47d5ad0ab777c667998934e
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-26 18:21:59 +0530

    Update variables.tf

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-southeast-2"
+  default     = "ap-south-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "new-south"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,18 +12,18 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-09b0a86a2c84101e1"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "purplehaze777"
+  default     = "jenkinscicd"
 }
 
 variable "dynamodb_table" {
   description = "The name of the dynamodb table"
   type        = string
-  default     = "purplehaze777-dynamodb-table"
+  default     = "jenkinscicd-dynamodb-table"
 }
 
 variable "acl" {
