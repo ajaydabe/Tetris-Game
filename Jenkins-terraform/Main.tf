4e88a01eee38d7a44ea97ced1189bf0b247a429c
ajaydabe <ajaydabe1996@gmail.com>
2024-11-28 06:56:28 +0000

    update arg

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -62,7 +62,6 @@ resource "aws_instance" "web" {
   instance_type          = "t2.large"
   key_name               = "new-south"
   subnet_id              = "subnet-05b0c58a7e6da9403"
-  map_public_ip_on_launch = true
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
