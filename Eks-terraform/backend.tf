b6b4317df879ce6298a4812457603e3ac4b9e899
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-29 11:27:18 +0530

    Delete Eks-terraform directory

--- Eks-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tetrisbucket1996" # Replace with your actual S3 bucket name
-    key    = "EKSTerraform/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
