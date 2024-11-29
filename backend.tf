8acca56b619c41ef4a81118407e61e8891df2da6
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-29 12:18:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "var.bucket_name"
-    key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "var.dynamodb_table"
+    bucket         = var.bucket_name
+    key            = "JenkinsCICD/main"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table
   }
 }
