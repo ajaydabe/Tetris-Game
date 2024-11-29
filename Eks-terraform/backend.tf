07530d4088996cc9aecd460b2093c0e62fceb93d
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-29 10:39:59 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetrisbucket1996" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "EKSTerraform/terraform.tfstate"
     region = "ap-south-1"
   }
 }
