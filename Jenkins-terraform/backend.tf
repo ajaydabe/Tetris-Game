21b9227474ca2c394a59402ebbb009a74e01b2cb
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-11-29 11:28:16 +0530

    Delete Jenkins-terraform directory

--- Jenkins-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tetrisbucket1996"    # Replace with your actual S3 bucket name
-    key    = "JenkinsTerraform/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
